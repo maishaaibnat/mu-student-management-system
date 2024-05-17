a725c4ec9b11a4eb1dcbd7ec4ac492104fb42029
Farhaan mahbub <123767495+maishaaibnat@users.noreply.github.com>
2024-05-18 00:57:31 +0600

    Final Project Push

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/farhanmahbub/Documents/bin/broken-files/panna/student-management-system/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
